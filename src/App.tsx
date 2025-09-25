099925600c17b81bb992d4848adf41e9d16ce69a
HemantaRegmi <hemantaregmi20@gmail.com>
2025-09-25 02:25:52 -0400

    2.31

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <NavBar></NavBar>
-      <Home></Home>
+      <Home></Home>n
     </>
   );
 }
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,12 @@
 export default function About() {
   return (
     <div>
-      <h1 className="flex h-full">Welcome to my Porfolio Page</h1>
-      <h2>My name is Hemanta Regmi</h2>
+      <h1 className="flex h-full justify-center ">
+        Welcome to my Porfolio Page
+      </h1>
+      <>
+        <h2>My name is Hemanta Regmi.</h2>
+      </>
     </div>
   );
 }
